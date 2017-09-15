a500785ac097c8cd5fbe7cb8d18055030674ac5d
Michael Hodis <mhodis@zeuselectronics.eu>
2017-09-15 04:55:51 -0500

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-from psycopg2 import sql
-import sys
-from internetradio import RadioStreamsScrapper
-
-example = RadioStreamsScrapper('https://www.internet-radio.com/')
-categories = example.load_categories()
-
-example.update_db('rock', 123, 'irdb', 'godis_michael', '211195')
