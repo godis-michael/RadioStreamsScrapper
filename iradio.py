94db74783a3d3f7a9f949dbff12c719259081076
Michael Hodis <godismichael1995@gmail.com>
2017-09-19 16:29:01 +0300

    - Added UPDATE func for incremental updating of db

--- iradio.py
+++ iradio.py
@@ -23,7 +23,7 @@ class RadioStreamsScrapper:
 
     def __check_url(self):
 
-        """ Checks url for correct ending. If no backslash provided at the end of url, it will be automatically
+        """ Check url for correct ending. If no backslash provided at the end of url, will be automatically
         added. """
 
         if self.url[-1] != '/':
@@ -34,7 +34,7 @@ class RadioStreamsScrapper:
     @staticmethod
     def _load_page(url):
 
-        """ Loads page and returns it as a soup object. """
+        """ Load page and return it as a soup object. """
 
         try:
             open_page = requests.get(url).text
@@ -45,7 +45,7 @@ class RadioStreamsScrapper:
 
     def load_categories(self, tag='div', class_name='panel-body', category_tag='a'):
 
-        """ Returns list of site categories available to process. """
+        """ Return list of site categories available to process. """
 
         bar = pb.ProgressBar()
         categories = []
@@ -59,28 +59,28 @@ class RadioStreamsScrapper:
         return categories
 
     @staticmethod
-    def _get_streams_num(soup, tag='p', class_name='lead'):
+    def _get_streams_num(soup, tag='p', _class='lead'):
 
         """ Number of streams available to download for certain category. """
 
-        paragraph = soup.find(tag, attrs={'class': class_name})
+        paragraph = soup.find(tag, attrs={'class': _class})
         streams_num = re.search(r'\d+', paragraph.text)
 
         return int(streams_num.group(0))
 
     @staticmethod
-    def _get_pages_num(soup, tag='li', class_name='next'):
+    def _get_pages_num(soup, tag='li', _class='next'):
 
-        """ Number of pages for certain category. """
+        """ Number of paginated pages for certain category. """
 
         try:
-            return int(soup.find(tag, attrs={'class': class_name}).previous_sibling.text)
+            return int(soup.find(tag, attrs={'class': _class}).previous_sibling.text)
         except AttributeError:
             return 1
 
     def _create_temp_dir(self):
 
-        """ Creates temporary directory for downloaded streams. """
+        """ Create temporary directory for downloaded streams. """
 
         self._remove_temp_dir()
         os.makedirs('tmp')
@@ -88,7 +88,7 @@ class RadioStreamsScrapper:
     @staticmethod
     def _remove_temp_dir():
 
-        """ Removes temporary directory. """
+        """ Remove temporary directory. """
 
         if os.path.exists('tmp'):
             shutil.rmtree('tmp')
@@ -96,7 +96,7 @@ class RadioStreamsScrapper:
     @staticmethod
     def _numerate_streams(array):
 
-        """ Creates additional column in array with streams id`s. """
+        """ Create additional column in array with streams id`s. """
 
         rows = array.shape[0]
         numeration = np.arange(1, rows + 1).reshape(-1, 1)
@@ -107,13 +107,13 @@ class RadioStreamsScrapper:
     @staticmethod
     def _create_tuple(array):
 
-        """ Converts array into tuple (for loading into database). """
+        """ Convert array into tuple (for loading into database). """
 
         return tuple([tuple(row) for row in array])
 
     def _download_streams(self, soup, streams=np.empty(shape=(0, 2))):
 
-        """ Downloads all streams from a page. """
+        """ Download all streams from a page. """
 
         self._create_temp_dir()
         index = 1
@@ -139,7 +139,7 @@ class RadioStreamsScrapper:
 
     def get_streams(self, category, numerated=False, tupled=False):
 
-        """ Gets all streams for certain category. Streams will be numerated and(or) converted to tuple if
+        """ Get all streams for certain category. Streams will be numerated and(or) converted to tuple if
         'numerated'/'tupled' is set to True. """
 
         print('\nStarting download for category "%s"' % category)
@@ -168,23 +168,29 @@ class RadioStreamsScrapper:
         return streams
 
     @staticmethod
-    def db_cascade(operations, dbname, user, password, host='localhost'):
+    def init_db_connection(operations, dbname, user, password, host='localhost'):
+
+        """ Cascade for opening connection with database. To perform operations to database pass a list of operations
+        as first parameter.
+
+            For example:
+            [[method_name, parametr1, parametr2],
+             [method_name2, parametr1, parametr2, parametr3]] """
+
         con = None
 
         try:
             con = psycopg2.connect("host='%s' dbname='%s' user='%s' password='%s'" % (host, dbname, user, password))
-            print('Opened database successfully')
+            print('Opened database connection successfully\n')
             cur = con.cursor()
 
             for op in operations:
-                if len(op) == 2:
-                    func = op[0]
-                    arg = op[1]
-                    func(con, cur, arg)
-                elif len(op) == 4:
-                    func = op[0]
-                    arg1,arg2,arg3 = op[1:]
-                    func(con, cur, arg1, arg2, arg3)
+                func = op[0]
+                if len(op) > 1:
+                    params = op[1:]
+                    func(cur, *params)
+                else:
+                    func(cur)
 
             con.commit()
 
@@ -200,33 +206,68 @@ class RadioStreamsScrapper:
                 con.close()
 
     @staticmethod
-    def create_db_schema(con, cur, schema_name):
-        print('Creating new schema in database with \'{}\' label'.format(schema_name))
+    def create_schema(cur, name):
+
+        """ Create new schema in database """
+
+        print('Creating new schema in database with \'{}\' label'.format(name))
 
         cur.execute("SELECT schema_name FROM information_schema.schemata")
         exist_schemas = cur.fetchall()
 
-        drop_schema_query = sql.SQL("DROP SCHEMA {} CASCADE").format(sql.Identifier(schema_name))
-        create_schema_query = sql.SQL("CREATE SCHEMA {}").format(sql.Identifier(schema_name))
+        drop_schema_query = sql.SQL("DROP SCHEMA {} CASCADE").format(sql.Identifier(name))
+        create_schema_query = sql.SQL("CREATE SCHEMA {}").format(sql.Identifier(name))
 
-        if (schema_name,) in exist_schemas:
+        if (name,) in exist_schemas:
             cur.execute(drop_schema_query)
             cur.execute(create_schema_query)
         else:
             cur.execute(create_schema_query)
 
-        print('\'{}\' schema has been successfully created'.format(schema_name))
+        print('\'{}\' schema has been successfully created'.format(name))
 
     @staticmethod
-    def load_into_db(con, cur, schema_name, table_name, data):
+    def get_schemas(dbname, user, password, host='localhost'):
+
+        """ Show available database schemas  """
+
+        con = None
+
+        try:
+            con = psycopg2.connect("host='%s' dbname='%s' user='%s' password='%s'" % (host, dbname, user, password))
+            cur = con.cursor()
+
+            cur.execute("SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN "
+                        "('information_schema', 'public') AND schema_name !~ E'^pg_' ")
+            exist_schemas = cur.fetchall()
+            exist_schemas = [schema[0] for schema in exist_schemas]
+
+            return exist_schemas
+
+        except psycopg2.DatabaseError as e:
+            if con:
+                con.rollback()
+
+            print('Error %s' % e)
+            sys.exit(1)
+
+        finally:
+            if con:
+                con.close()
+
+    @staticmethod
+    def load_into_db(cur, schema_name, table_name, data):
+
+        """ Load data into a database  """
+
         print('Loading category \'{}\' into database...'.format(table_name))
 
-        dest = '.'.join([schema_name, table_name])
-        drop_query = sql.SQL("DROP TABLE IF EXISTS {}").format(sql.Identifier(dest))
-        create_query = sql.SQL("CREATE TABLE {}(Id INT PRIMARY KEY, Name TEXT, Link text)").format(
-            sql.Identifier(dest))
-        insert_query = sql.SQL("INSERT INTO {} (id, Name, Link) VALUES (%s, %s, %s)").format(
-            sql.Identifier(dest))
+        drop_query = sql.SQL("DROP TABLE IF EXISTS {}")\
+            .format(sql.SQL('.').join([sql.Identifier(schema_name), sql.Identifier(table_name)]))
+        create_query = sql.SQL("CREATE TABLE {}(Id INT PRIMARY KEY, Name TEXT, Link text)")\
+            .format(sql.SQL('.').join([sql.Identifier(schema_name), sql.Identifier(table_name)]))
+        insert_query = sql.SQL("INSERT INTO {}(id, Name, Link) VALUES (%s, %s, %s)")\
+            .format(sql.SQL('.').join([sql.Identifier(schema_name), sql.Identifier(table_name)]))
 
         cur.execute(drop_query)
         cur.execute(create_query)
@@ -234,130 +275,47 @@ class RadioStreamsScrapper:
 
         print('Finished loading')
 
-    # @staticmethod
-    # def create_db_schema(dbname, user, password, host='localhost'):
-    #     name = '{:%Y-%b-%d %H:%M}'.format(datetime.now())
-    #     con = None
-    #
-    #     try:
-    #         print('Creating new image for data with \'{}\' label...'.format(name))
-    #         con = psycopg2.connect("host='%s' dbname='%s' user='%s' password='%s'" % (host, dbname, user, password))
-    #         print('Opened database successfully')
-    #         cur = con.cursor()
-    #
-    #         cur.execute("SELECT schema_name FROM information_schema.schemata")
-    #         exist_schemas = cur.fetchall()
-    #
-    #         drop_schema_query = sql.SQL("DROP SCHEMA {} CASCADE").format(sql.Identifier(name))
-    #         create_schema_query = sql.SQL("CREATE SCHEMA {}").format(sql.Identifier(name))
-    #
-    #         if (name,) in exist_schemas:
-    #             cur.execute(drop_schema_query)
-    #             cur.execute(create_schema_query)
-    #         else:
-    #             cur.execute(create_schema_query)
-    #
-    #         con.commit()
-    #         # print('Finished loading')
-    #
-    #     except psycopg2.DatabaseError as e:
-    #         if con:
-    #             con.rollback()
-    #
-    #         print('Error %s' % e)
-    #         sys.exit(1)
-    #
-    #     finally:
-    #         if con:
-    #             con.close()
+    @staticmethod
+    def _show_tables(cur, schema_name):
 
-    # @staticmethod
-    # def load_into_db(table_name, data, dbname, user, password, host='localhost'):
-    #
-    #     """ Loads data provided into a database. If table for category is already exist it will be overridden. """
-    #
-    #     con = None
-    #
-    #     try:
-    #         print('Loading category \'{}\' into database...'.format(table_name))
-    #         con = psycopg2.connect("host='%s' dbname='%s' user='%s' password='%s'" % (host, dbname, user, password))
-    #         print('Opened database successfully')
-    #         cur = con.cursor()
-    #
-    #         drop_query = sql.SQL("DROP TABLE IF EXISTS {}").format(sql.Identifier(table_name))
-    #         create_query = sql.SQL("CREATE TABLE {}(Id INT PRIMARY KEY, Name TEXT, Link text)").format(
-    #             sql.Identifier(table_name))
-    #         insert_query = sql.SQL("INSERT INTO {} (id, Name, Link) VALUES (%s, %s, %s)").format(
-    #             sql.Identifier(table_name))
-    #
-    #         cur.execute(drop_query)
-    #         cur.execute(create_query)
-    #         cur.executemany(insert_query, data)
-    #
-    #         con.commit()
-    #         print('Finished loading')
-    #
-    #     except psycopg2.DatabaseError as e:
-    #         if con:
-    #             con.rollback()
-    #
-    #         print('Error %s' % e)
-    #         sys.exit(1)
-    #
-    #     finally:
-    #         if con:
-    #             con.close()
+        """ List of available tables for certain schema """
 
-    # @staticmethod
-    # def update_db(table_name, data, dbname, user, password, host='localhost'):
-    #
-    #     """ Compares category data with existing table. If category does not exist among tables, it will be
-    #     downloaded. """
-    #
-    #     con = None
-    #
-    #     try:
-    #         print('Updating category \'{}\'...'.format(table_name))
-    #         con = psycopg2.connect("host='%s' dbname='%s' user='%s' password='%s'" % (host, dbname, user, password))
-    #         print('Opened database successfully')
-    #         cur = con.cursor()
-    #
-    #         cur.execute("SELECT table_name FROM information_schema.tables "
-    #                     "WHERE table_type = 'BASE TABLE' AND table_schema = 'public' "
-    #                     "ORDER BY table_type, table_name")
-    #         exist_tables = cur.fetchall()
-    #
-    #         if (table_name,) in exist_tables:
-    #             select_query = sql.SQL("SELECT * FROM {}").format(sql.Identifier(table_name))
-    #             cur.execute(select_query)
-    #             exist_data = cur.fetchall()
-    #             cache = set(itertools.chain.from_iterable(exist_data))
-    #             num_objects = exist_data[-1][0]
-    #
-    #             print(cache)
-    #             # for row in data:
-    #             #     if row[1] in cache
-    #
-    #         else:
-    #             print('Category \'{}\' does not exist in database. Starting load...')
-    #             create_query = sql.SQL("CREATE TABLE {}(Id INT PRIMARY KEY, Name TEXT, Link text)").format(
-    #                 sql.Identifier(table_name))
-    #             insert_query = sql.SQL("INSERT INTO {} (id, Name, Link) VALUES (%s, %s, %s)").format(
-    #                 sql.Identifier(table_name))
-    #
-    #             cur.execute(create_query)
-    #             cur.executemany(insert_query, data)
-    #
-    #             con.commit()
-    #             print('Finished loading')
-    #
-    #     except psycopg2.DatabaseError as e:
-    #         if con:
-    #             con.rollback()
-    #
-    #         print('Error %s' % e)
-    #         sys.exit(1)
-    #
-    #     finally:
-    #         if con:
-    #             con.close()
+        query = "SELECT table_name FROM information_schema.tables WHERE table_schema = %s"
+        cur.execute(query, (schema_name,))
+        exist_tables = cur.fetchall()
+        exist_tables = [table[0] for table in exist_tables]
+
+        return exist_tables
+
+    def update_db(self, cur, schema, dest='public'):
+
+        """ 1. Add new tables (if don`t exist) to result(public) schema from other stored schema
+            2. Add new streams to result schema if they don`t exist
+            3. Rename stream in the result schema if name differs from one in stored schema """
+
+        schema_tables = self._show_tables(cur, schema)
+        public_tables = self._show_tables(cur, dest)
+
+        bar = pb.ProgressBar()
+        print('Making update from \'{}\' schema...'.format(schema))
+        for table in bar(schema_tables):
+            if table in public_tables:
+                cur.execute(sql.SQL("SELECT * FROM {}").format(sql.SQL(".").join([sql.Identifier(schema), sql.Identifier(table)])))
+                schema_data = cur.fetchall()
+                schema_data = {row[2]:row[1] for row in schema_data}
+
+                cur.execute(sql.SQL("SELECT * FROM {}").format(sql.Identifier(table)))
+                public_data = cur.fetchall()
+                index = max([item[0] for item in public_data]) + 1
+                public_data = {row[2]:row[1] for row in public_data}
+
+                for link,name in schema_data.items():
+                    if link in public_data and name != public_data[link]:
+                        query = sql.SQL("UPDATE {} SET name=%s WHERE link=%s").format(sql.Identifier(table))
+                        cur.execute(query, (name, link))
+                    elif link not in public_data:
+                        query = sql.SQL("INSERT INTO {}(id, Name, Link) VALUES (%s, %s, %s)").format(sql.Identifier(table))
+                        data = (index, name, link)
+                        # print(type(index))
+                        cur.execute(query, data)
+                        index += 1
